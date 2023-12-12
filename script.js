1dd2a98ee44103499584d22f42d5ecb2d4bed213
vitnsk <121999199+vitnsk@users.noreply.github.com>
2023-12-12 12:42:21 +0700

    finaly_7

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let slide;
 
+if(document.querySelector("#imgST")||document.querySelector("#imgCalc")||document.querySelector("#imgTDS"))
+{
 transformImg(document.querySelector("#imgST"));
 transformImg(document.querySelector("#imgCalc"));
 transformImg(document.querySelector("#imgTDS"));
-
+}
 function transformImg(slide) {
 slide.addEventListener("mouseover", function() {
 slide.style.transform = "scale(1.1)";
